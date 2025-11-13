13a434cf3107f899e862b94276e03e2e51c885ea
thierryvdb <cloudyvdb@gmail.com>
2025-11-13 08:37:34 -0300

    ajustes

--- backend/src/routes/strikes.ts
+++ backend/src/routes/strikes.ts
@@ -141,8 +141,8 @@ export async function strikesRoutes(app: FastifyInstance) {
           $29,$30,
           $31,$32,$33,
           $34,$35,$36,$37,$38,$39,$40,$41,$42,
-          $43,
-          $44,$45
+          $43,$44,
+          $45,$46,$47
         ) RETURNING strike_id AS id` ,
         [
           body.airport_id,
